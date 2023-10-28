f55533e3f8b16505ec5a8449020a97b44f47a088
Indra <igopali049@gmail.com>
2023-10-27 21:25:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//check if a number is prime on JavaScript
+function isPrime(num){
+    if(num<=1) return false;
+    
+    for(let i=2; i<num; i++){
+        if(num % i === 0){
+        return false;
+        }
+    }
+    return true;
+}
+isPrime(5)
